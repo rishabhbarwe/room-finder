c4cec0cee52bb48709c04b78df90d6675135bc45
rishabh-barwe <risabhbarwe512@gmail.com>
2025-06-12 09:48:03 +0530

    Deployed1

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files on Render
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 # ---------------------
 # CORS (Cross-Origin)
